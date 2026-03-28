1163d3faae84958146f8e0e46886f6f2fff9e640
KarkRio <termuxkark@gmail.com>
2026-03-28 19:59:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cortexos-v15.0";
+const CACHE = "cortexos-v15.1";
 const ASSETS = ["./index.html","./manifest.json"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
